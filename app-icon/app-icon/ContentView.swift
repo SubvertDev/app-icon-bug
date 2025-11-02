c26fccb2aada00db62a8e04a5fc6866c2c277251
Ilia Lubianoi <59670026+SubvertDev@users.noreply.github.com>
2025-11-02 16:27:30 +0300

    Initial commit

---
+++ app-icon/app-icon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  app-icon
+//
+//  Created by Ilia Lubianoi on 02.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app-icon/app-icon/app_iconApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_iconApp.swift
+//  app-icon
+//
+//  Created by Ilia Lubianoi on 02.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct app_iconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
